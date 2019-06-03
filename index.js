482e88c516925569001ab7eeccaee13967f22c34
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-03 11:21:22 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  changeText()
+});
+
+function changeText () {
+  document.querySelector("#text").innerHTML = "This is really cool!"
+};
