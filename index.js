70e5052036d3e9506eab70dd07776f6dca4407fd
MarcusMellorMIM <marcus@mellor.com>
2019-06-04 08:17:17 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textId = document.getElementById('text');
+    textId.innerHTML = "This is really cool!";
+}); 
